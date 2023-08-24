1dc5d49eb53e524ddddde6376675d6ed4ec7590f
corffen-win <corffen@gmail.com>
2023-08-24 21:16:11 +0800

    新增 单例模式饿汉式

---
+++ src/main/kotlin/com/corffen/kdp/singleton/App.kt
@@ -0,0 +1,9 @@
+package com.corffen.kdp.singleton
+
+fun main() {
+    val instance1 = IvoryTower
+    val instance2 = IvoryTower
+
+    println("ivory instance 1 = $instance1")
+    println("ivory instance 2 = $instance2")
+}
